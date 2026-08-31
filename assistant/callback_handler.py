24891dadb851538f3ca50e924c5ba7a86b19aeae
zp <inizulpii25@gmail.com>
2026-08-31 09:34:20 +0800

    Add waifu_next and waifu_close callbacks

--- assistant/callback_handler.py
+++ assistant/callback_handler.py
@@ -13,7 +13,7 @@ from command import (acc_page, back_home,
                      refresh_cat, reset_costum_text, reset_emoji, reset_prefix,
                      rest_anime, restart_userbot,
                      selected_topic, token_cmd, tools_acc, tools_token,
-                     tools_userbot, user_aggre, viewchord, viewgempa)
+                     tools_userbot, user_aggre, viewchord, viewgempa, waifu_next, waifu_close)
 from helpers import CMD, trigger
 from logs import logger
 
@@ -135,6 +135,11 @@ async def _(client, callback):
 
         elif query.startswith("tools_acc"):
             return await tools_acc(client, callback)
+        elif query.startswith("waifu_next"):
+            return await waifu_callback(client, callback)
+
+        elif query.startswith("waifu_close"):
+            return await waifu_close(client, callback)
         elif query.startswith("acc_page"):
             return await acc_page(client, callback)
         elif query.startswith("bcpg_acc"):
