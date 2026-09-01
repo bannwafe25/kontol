d476c70c05e17d446a15b642f856573d1b375566
zp <inizulpii25@gmail.com>
2026-09-02 05:20:03 +0800

    Change default XKIRO_MODEL to deepseek-v4-flash

--- config/config.py
+++ config/config.py
@@ -59,7 +59,7 @@ XKIRO_BASE_URL = os.environ.get(
 )
 XKIRO_MODEL = os.environ.get(
     "XKIRO_MODEL",
-    "minimax/minimax-m2.7-highspeed:free"
+    "deepseek/deepseek-v4-flash"
 )
 API_MAELYN = os.environ.get("API_MAELYN", "")
 
