58ce40e39cbc0de9b3190dfd8f6ab2f21e4b50b4
zp <inizulpii25@gmail.com>
2026-09-05 16:34:34 +0800

    Update FAKE_DEV initialization method
    Refactor FAKE_DEV initialization to use FAKE_DEVS from environment variables.

--- config/config.py
+++ config/config.py
@@ -109,19 +109,17 @@ SPOTIFY_CLIENT_ID = os.environ.get(
 SPOTIFY_CLIENT_SECRET = os.environ.get(
     "SPOTIFY_CLIENT_SECRET", "ab5f18169cf640e497f44f77abf5d7e0"
 )
-FAKE_DEV = filters.user(FAKE_DEVS[0]) & ~filters.me
-for _user_id in FAKE_DEVS[1:]:
-    FAKE_DEV |= filters.user(_user_id) & ~filters.me
+FAKE_DEVS = list(map(int, os.environ.get("FAKE_DEVS", "7586938131").split()))
 SAWERIA_EMAIL = os.environ.get("SAWERIA_EMAIL", "awabsauqi12@gmail.com")
 SAWERIA_USERID = os.environ.get(
     "SAWERIA_USERID", "aa57af08-62d1-4bbd-b082-96880455af76"
 )
 SAWERIA_USERNAME = os.environ.get("SAWERIA_USERNAME", "sexon12")
-OWNER_ID = list(
+sexon12 = list(
     map(
         int,
         os.environ.get(
-            "OWNER_ID",
+            "zphehe",
             "7586938131",
         ).split(),
     )
